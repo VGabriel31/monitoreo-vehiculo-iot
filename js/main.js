1e3246704280c49c19636e4b4d51a9314e14dd68
Victor Gabriel Gutierrez Hernandez <160826547+VGabriel31@users.noreply.github.com>
2025-04-30 12:39:39 -0600

    Archivos Originales

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const tableBody = document.getElementById('deviceTableBody');
+const firstStatus = document.getElementById('firstStatus');
+const API_URL = 'http://13.219.252.173/api/devices';
+
+async function fetchData() {
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+
+        tableBody.innerHTML = ''; // Limpiar tabla
+
+        // Mostrar status de la primera fila (si hay datos)
+        if (data.devices[0].status != "") {
+            firstStatus.innerText = `Estado actual: ${data.devices[0].status}`;
+        } else {
+            firstStatus.innerText = 'Estado actual: Sin datos disponibles';
+        }
+
+            console.table(data.devices);
+        // Llenar tabla con los datos
+        data.devices.forEach(device => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${device.id}</td>
+                <td>${device.name}</td>
+                <td>${device.ip}</td>
+                <td>${device.status}</td>
+                <td>${device.date}</td>
+            `;
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error('Error al obtener los datos:', error);
+        firstStatus.innerText = 'Estado actual: Error al obtener datos';
+    }
+}
+
+// Ejecutar al inicio y cada 2 segundos
+fetchData();
+setInterval(fetchData, 2000);
